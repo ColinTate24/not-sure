2658d1664df49e6c814fcc40774cfcc4c9584f66
Tate <ctate2@una.edu>
2022-10-13 13:49:10 -0500

    Made progress, maybe

--- app.js
+++ app.js
@@ -5,31 +5,44 @@ const { urlencoded } = require('body-parser');
 const MongoClient = require('mongodb').MongoClient
 const app = express();
 const { ObjectId } = require('mongodb');
+const PORT = (process.env.PORT || 3000)
 
 dotenv.config({path:'config.env'});
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', async function (req, res) {
+// app.get('/', async function (req, res) {
     // res.sendFile(path.join(__dirname, "index.html" )); 
     // res.send('Hello ' + userName + ' from Node/Express/Heroku');
 
-    client.connect;
+    MongoClient.connect(`mongodb+srv:Colingarrett24:<RdrZcoTWuMploJes>@cluster0.khllp7s.mongodb.net/?retryWrites=true&w=majority` ,
+    {useUnifiedTopology: true}) 
+        .then(client =>{
+          console.log('connected to db')
+          const db = client.db('roster')
+          const playersCollection = db.collection('players')  
+          
+          app.set('view engine', 'ejs')
+          app.use(bodyParser.urlencoded({ extended: true }))
+          app.use(bodyParser.json());
+          app.use(express.static('public'))});
 
-        const collection = client.db("Cluster0").collection("posts");
-        console.log('connected!');
-        // perform actions on the collection object
+    // client.connect;
+
+    //     const collection = client.db("Cluster0").collection("posts");
+    //     console.log('connected!');
+    //     // perform actions on the collection object
        
-        console.log('closed!');
+    //     console.log('closed!');
 
     
-        const result = await collection.findOne(); //.toArray();
-         console.log(result.titlenp);
+    //     const result = await collection.findOne(); //.toArray();
+    //      console.log(result.titlenp);
             
-            res.send(result);
-            // client.close();
+    //         res.send(result);
+    //         // client.close();
         
-    });
+    // });
 
         // res.send(`Hello Express from inside my client connect f/n!`);
     
@@ -37,9 +50,4 @@ app.get('/', async function (req, res) {
     // res.render('index',  {     }    );
 
 
-app.listen(process.env.PORT || 3000,
-  () => console.log(`server is running on port: ${process.env.PORT}` ));
-
-
-
-
+app.listen(PORT, console.log(`server is running on port: ${PORT}` ));
