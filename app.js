e60da9073261916a8b5556ebb8d2487797c327a7
Tate <ctate2@una.edu>
2022-10-13 13:17:57 -0500

    moving things, adding files

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-require("dotenv").config();
-const express = require('express')
-const path = require('node:path')
-const bodyParser = require('body-parser')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
+const app = express();
+const { ObjectId } = require('mongodb');
 
-const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+dotenv.config({path:'config.env'});
 
-const app = express()
-
-app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.set('view engine', 'ejs');
 
 app.get('/', async function (req, res) {
     // res.sendFile(path.join(__dirname, "index.html" )); 
